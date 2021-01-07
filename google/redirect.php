5f57f80b4b247350da50642d3c765df5a63543a5
Blackscorp <cccpmik@gmail.com>
2021-01-07 17:52:26 +0100

    link erstellt

---
+++ google/redirect.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__.'/../bootstrap.php';
+
+$_SESSION['oauth2state'] =  $provider->getState();
+
+$authorizationUrl = $provider->getAuthorizationUrl();
+header('Location:'.$authorizationUrl);
